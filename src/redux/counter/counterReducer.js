0344ff5f4df179cab88bb1a30611ef73940526cd
Ashraf59 <ashrafams59b@gmail.com>
2023-02-26 12:09:43 +0600

    dynamic value setup

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -10,13 +10,13 @@ const counterReducer = (state = initialState, action) => {
 
         return{
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         };
         case DECREMENT: 
 
         return{
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         };
 
         default:
